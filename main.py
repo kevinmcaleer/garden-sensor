f867ff42ae239cd41f53418fe15ec86e024b0689
Kevin McAleer <kevinmcaleer@gmail.com>
2025-05-30 13:08:09 +0100

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ message = ""
 sound = 0
 brightness = 0
 temp = 0
+datalogger.set_column_titles("temp", "light", "sound")
 radio.set_group(42)
-
 while True:
     temp = input.temperature()
     brightness = input.light_level()
     sound = input.sound_level()
-    message = "T:{temp},L:{light},S:{sound}"
+    datalogger.log(datalogger.create_cv("light", brightness),
+        datalogger.create_cv("temp", temp),
+        datalogger.create_cv("sound", sound))
+    message = "T: " + ("" + str(temp)) + ", L: " + ("" + str(brightness)) + ", S: " + ("" + str(sound))
     radio.send_string(message)
     basic.show_string("S")
-    
     # Indicate sending
     wait_in_seconds = 2000
     basic.clear_screen()
--- main.ts
+++ main.ts
@@ -3,12 +3,22 @@ let message = ""
 let sound = 0
 let brightness = 0
 let temp = 0
+datalogger.setColumnTitles(
+"temp",
+"light",
+"sound"
+)
 radio.setGroup(42)
 while (true) {
     temp = input.temperature()
     brightness = input.lightLevel()
     sound = input.soundLevel()
-    message = "T:{temp},L:{light},S:{sound}"
+    datalogger.log(
+    datalogger.createCV("light", brightness),
+    datalogger.createCV("temp", temp),
+    datalogger.createCV("sound", sound)
+    )
+    message = "T: " + ("" + temp) + ", L: " + ("" + brightness) + ", S: " + ("" + sound)
     radio.sendString(message)
     basic.showString("S")
     // Indicate sending
